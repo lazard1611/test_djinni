684f20c7a39c1598571e91a035e03a37136523c3
Lazar <lazard1611@gmail.com>
2023-04-15 14:13:56 +0300

    add scroll fn

--- src/js/components/infinite-scroll.js
+++ src/js/components/infinite-scroll.js
@@ -1,8 +1,18 @@
 const infiniteScroll = () => {
+	const activeClass = 'load_dots--load_state';
+	const $dots = document.querySelector('.js-dots-load');
+	if (!$dots) return;
+	let isScrollBottom = true;
+
 	window.addEventListener('scroll', () => {
 		const { clientHeight, scrollHeight, scrollTop } = document.documentElement;
-		if (scrollHeight < scrollTop + clientHeight + 11) {
-			console.log('load');
+		if (scrollHeight <= scrollTop + clientHeight && isScrollBottom) {
+
+			isScrollBottom = false;
+			$dots.classList.add(activeClass);
+		} else {
+			isScrollBottom = true;
+			$dots.classList.remove(activeClass);
 		}
 	});
 };
